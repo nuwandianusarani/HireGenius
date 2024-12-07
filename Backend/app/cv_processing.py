95876d5ecd2d33152c683793c312442779103a20
Kushan Nalinka <kushannalinka572@gmail.com>
2024-12-07 21:58:41 +0530

    Cv data Extraction and Model Prediction Integration
    Resume data Extraction and Model Prediction Integration

--- Backend/app/cv_processing.py
+++ Backend/app/cv_processing.py
@@ -50,8 +50,39 @@ def extract_soft_skills(cv_text):
                 soft_skills_found.append(label)
     return Counter(soft_skills_found).most_common()
 
+# Define dictionaries for technical skills
+skills_keywords = {
+    'Programming Languages': [
+        'Python', 'Java', 'C', 'C++', 'C#', 'JavaScript', 'TypeScript', 'PHP', 'Ruby', 'Go', 'Swift', 'Kotlin', 'Rust'
+    ],
+    'Frameworks and Libraries': [
+        'React', 'Angular', 'Node.js', 'Django', 'Flask', 'Spring', 'Laravel', 'Vue.js', 'Bootstrap', 'TensorFlow',
+        'Keras', 'PyTorch', 'jQuery', 'Express', 'Next.js', 'Spring Boot', 'Material UI', 'Hibernate', 'FastAPI'
+    ],
+    'Databases': [
+        'MySQL', 'MongoDB', 'PostgreSQL', 'SQLite', 'Oracle', 'SQL Server', 'Firebase', 'DynamoDB', 'Redis'
+    ],
+    'Cloud Platforms': [
+        'AWS', 'Azure', 'Google Cloud', 'Firebase', 'Heroku', 'DigitalOcean', 'Cloudflare', 'IBM Cloud'
+    ],
+    'Tools and Technologies': [
+        'Docker', 'Kubernetes', 'Git', 'GitHub', 'GitLab', 'JIRA', 'Bitbucket', 'Jenkins', 'Ansible', 'Terraform',
+        'CI/CD', 'Serverless', 'Kibana', 'Elasticsearch', 'Logstash', 'Splunk', 'Figma', 'Postman'
+    ],
+    'Development Methodologies': [
+        'Agile', 'Scrum', 'Kanban', 'DevOps', 'Waterfall', 'Test-Driven Development', 'Behavior-Driven Development'
+    ]
+}
+
+# Combine all technical skills into a single list for easier matching
+all_technical_skills = [skill for category in skills_keywords.values() for skill in category]
+
+    
+
 # Function to extract technical skills from project descriptions
 def extract_technical_skills_from_projects(text):
+
+    
     doc = nlp(text.lower())
     extracted_technical_skills = []
     for sentence in doc.sents:
@@ -87,5 +118,267 @@ def generate_and_store_skills_chart(skill_counts):
 
     return chart_base64
 
+def extract_programming_languages(text):
+    programming_languages = [
+        'Python', 'Java', 'C', 'C\\+\\+', 'C#', 'JavaScript', 'Ruby', 'Go', 'Swift', 'Kotlin', 'PHP',
+        'TypeScript', 'R', 'Perl', 'Objective-C', 'Rust', 'Scala', 'Dart', 'Haskell', 'MATLAB',
+        'Shell', 'PowerShell', 'SQL', 'Bash', 'HTML', 'CSS', 'SASS', 'Fortran', 'COBOL'
+    ]
+    
+    found_languages = []
+
+    programming_languages_escaped = [re.escape(lang) for lang in programming_languages]
+    pattern = re.compile(r'\b(' + '|'.join(programming_languages_escaped) + r')\b', re.IGNORECASE)
+    matches = pattern.findall(text)
+    found_languages.extend(set(matches))
+
+    doc = nlp(text)
+    for sent in doc.sents:
+        sentence_text = sent.text.lower()
+        for lang in programming_languages:
+            if lang.lower() in sentence_text and lang not in found_languages:
+                found_languages.append(lang)
+
+    return len(found_languages)
+
+def extract_website_development_technologies(text):
+    web_technologies = [
+        'HTML', 'CSS', 'JavaScript', 'TypeScript', 'PHP', 'Ruby', 'Python', 'Java', 'C#', 
+        'React', 'Angular', 'Vue.js', 'Svelte', 'Bootstrap', 'Tailwind CSS', 'jQuery', 
+        'Node.js', 'Express', 'Next.js', 'Nuxt.js', 'Django', 'Flask', 'Laravel', 'ASP.NET',
+        'Ruby on Rails', 'Spring', 'WordPress', 'Magento', 'Shopify', 'Joomla', 'Drupal',
+        'Firebase', 'MongoDB', 'MySQL', 'PostgreSQL', 'SQLite', 'Redis', 'GraphQL', 'REST API',
+        'WebSockets', 'Webpack', 'Gulp', 'Grunt', 'Parcel', 'Sass', 'LESS', 'JSON', 'AJAX',
+        'WebAssembly', 'Three.js', 'Babel', 'Handlebars', 'Pug', 'Materialize', 'Foundation'
+    ]
+    
+    found_web_technologies = []
+
+    pattern = re.compile(r'\b(' + '|'.join(web_technologies) + r')\b', re.IGNORECASE)
+    matches = pattern.findall(text)
+    found_web_technologies.extend(set(matches))
+
+    doc = nlp(text)
+    for sent in doc.sents:
+        sentence_text = sent.text.lower()
+        if 'web development' in sentence_text or 'website' in sentence_text:
+            for tech in web_technologies:
+                if tech.lower() in sentence_text and tech not in found_web_technologies:
+                    found_web_technologies.append(tech)
+
+    return len(found_web_technologies)
+
+def extract_programming_frameworks(text):
+    frameworks = [
+        'Django', 'Flask', 'React', 'Angular', 'Vue.js', 'Spring', 'Express', 'Rails', 'Ruby on Rails',
+        'Laravel', 'ASP.NET', 'Node.js', 'Next.js', 'Bootstrap', 'Tailwind CSS', 'Foundation',
+        'jQuery', 'Svelte', 'Nuxt.js', 'Redux', 'Keras', 'TensorFlow', 'Pandas', 'Hadoop',
+        'Spark', 'Vue', 'Symfony', 'Zend', 'CakePHP', 'Meteor', 'FastAPI', 'Quasar', 'Phoenix', 'Ionic',
+        'React Native', 'Electron', 'Flutter', 'Backbone.js', 'Alpine.js', 'NestJS', 'Struts', 'Sequelize',
+        'CodeIgniter', 'Play Framework', 'Vaadin', 'Gatsby', 'Ember.js', 'MobX', 'CouchDB', 'RxJava'
+    ]
+    
+    found_frameworks = []
+
+    pattern = re.compile(r'\b(' + '|'.join(frameworks) + r')\b', re.IGNORECASE)
+    matches = pattern.findall(text)
+    found_frameworks.extend(set(matches))
+
+    doc = nlp(text)
+    for sent in doc.sents:
+        sentence_text = sent.text.lower()
+        for framework in frameworks:
+            if framework.lower() in sentence_text and framework not in found_frameworks:
+                found_frameworks.append(framework)
+
+    return len(found_frameworks)
+
+def extract_cloud_technologies(text):
+    cloud_technologies = [
+        'AWS', 'Amazon Web Services', 'Azure', 'Google Cloud', 'GCP', 'IBM Cloud', 'Oracle Cloud',
+        'DigitalOcean', 'Alibaba Cloud', 'Salesforce', 'SAP Cloud', 'Cloud Foundry', 'Heroku', 
+        'Red Hat OpenShift', 'VMware Cloud', 'Rackspace', 'Linode', 'Cloudflare', 'Backblaze',
+        'Kubernetes', 'Docker', 'Terraform', 'Ansible', 'CloudFormation', 'Pulumi', 'Spinnaker', 
+        'Vault', 'Consul', 'Istio', 'Anthos', 'Cloud Functions', 'Lambda', 'Azure Functions', 
+        'Google Cloud Functions', 'Serverless', 'ECS', 'EKS', 'Fargate', 'AKS', 'OpenStack', 'VPC',
+        'EC2', 'S3', 'RDS', 'BigQuery', 'Cloud Storage', 'Cloud Run', 'Elastic Beanstalk', 
+        'CloudWatch', 'CloudTrail', 'CloudFront', 'IAM', 'Load Balancer', 'Auto Scaling', 'S3 Buckets'
+    ]
+    
+    found_technologies = []
+
+    pattern = re.compile(r'\b(' + '|'.join(cloud_technologies) + r')\b', re.IGNORECASE)
+    matches = pattern.findall(text)
+    found_technologies.extend(set(matches))
+
+    doc = nlp(text)
+    for sent in doc.sents:
+        sentence_text = sent.text.lower()
+        if 'cloud' in sentence_text or 'deployment' in sentence_text:
+            for tech in cloud_technologies:
+                if tech.lower() in sentence_text and tech not in found_technologies:
+                    found_technologies.append(tech)
+
+    return len(found_technologies)
+
+def extract_devops_technologies(text):
+    devops_tools = [
+        'Jenkins', 'GitLab CI', 'GitHub Actions', 'Travis CI', 'CircleCI', 'TeamCity', 'Bamboo',
+        'ArgoCD', 'Spinnaker', 'Flux', 'Harness', 'Azure DevOps', 'Octopus Deploy', 'CodePipeline',
+        'Docker', 'Kubernetes', 'Ansible', 'Terraform', 'Chef', 'Puppet', 'Nagios', 'Prometheus',
+        'Grafana', 'Elastic Stack', 'ELK Stack', 'Splunk', 'New Relic', 'AppDynamics', 'Sentry',
+        'Consul', 'Vault', 'Istio', 'Linkerd', 'Nginx', 'Apache Kafka', 'AWS CodeDeploy', 'CloudFormation',
+        'SaltStack', 'OpenShift', 'Vagrant'
+    ]
+    
+    found_tools = []
+
+    pattern = re.compile(r'\b(' + '|'.join(devops_tools) + r')\b', re.IGNORECASE)
+    matches = pattern.findall(text)
+    found_tools.extend(set(matches))
+
+    doc = nlp(text)
+    for sent in doc.sents:
+        sentence_text = sent.text.lower()
+        if 'ci/cd' in sentence_text or 'devops' in sentence_text:
+            for tool in devops_tools:
+                if tool.lower() in sentence_text and tool not in found_tools:
+                    found_tools.append(tool)
+
+    return len(found_tools)
+
+def extract_version_control_technologies(cv_text):
+    version_control_tools = [
+        "Git", "SVN", "Subversion", "Mercurial", "Perforce", "Bazaar", 
+        "GitHub", "GitLab", "Bitbucket", "Azure DevOps", "SourceForge",
+        "TortoiseSVN", "SmartGit", "GitKraken", "SourceTree"
+    ]
+    
+    vc_pattern = re.compile(r'\b(' + '|'.join(re.escape(tool) for tool in version_control_tools) + r')\b', re.IGNORECASE)
+    found_tools = set(vc_pattern.findall(cv_text))
+
+    doc = nlp(cv_text)
+    verified_tools = []
+    for sent in doc.sents:
+        sentence_text = sent.text.strip()
+        for tool in found_tools:
+            if tool.lower() in sentence_text.lower():
+                verified_tools.append(tool)
+    
+    verified_tools = list(set(verified_tools))
+    return len(verified_tools)
+
+def extract_database_technologies(cv_text):
+    database_technologies = [
+        "MySQL", "PostgreSQL", "SQLite", "Oracle Database", "SQL Server", "MongoDB", "Cassandra",
+        "DynamoDB", "CouchDB", "Redis", "Firebase", "Snowflake", "Redshift", "BigQuery",
+        "Neo4j", "JanusGraph", "ArangoDB", "Elasticsearch", "MariaDB", "CockroachDB", 
+        "HBase", "ClickHouse"
+    ]
+    
+    db_pattern = re.compile(r'\b(' + '|'.join(re.escape(db) for db in database_technologies) + r')\b', re.IGNORECASE)
+    found_databases = set(db_pattern.findall(cv_text))
+    
+    doc = nlp(cv_text)
+    verified_databases = []
+    for sent in doc.sents:
+        sentence_text = sent.text.strip()
+        for db in found_databases:
+            if db.lower() in sentence_text.lower():
+                verified_databases.append(db)
+    
+    verified_databases = list(set(verified_databases))
+    return len(verified_databases)
 
 
+def extract_software_development_methodologies(cv_text):
+    methodologies = [
+        'Agile', 'Scrum', 'Kanban', 'Waterfall', 'Lean', 
+        'Extreme Programming (XP)', 'DevOps', 'Spiral', 'RAD', 
+        'V-Model', 'Incremental Development', 'Iterative Development', 
+        'Prototyping'
+    ]
+
+    pattern = re.compile(r'\b(?:' + '|'.join(re.escape(method) for method in methodologies) + r')\b', re.IGNORECASE)
+
+    found_methodologies = pattern.findall(cv_text)
+
+    return len(list(dict.fromkeys(found_methodologies)))
+
+
+def extract_project_experiences(cv_text):
+    projects = []
+    project_keywords = ['project', 'technologies', 'tools', 'description', 'responsibilities']
+
+    lines = cv_text.split('\n')
+    current_project = None
+
+    for line in lines:
+        line_lower = line.lower().strip()
+        if any(keyword in line_lower for keyword in project_keywords) and line:
+            if current_project:
+                projects.append(current_project.strip())
+            current_project = line.strip()
+        elif current_project:
+            current_project += " " + line.strip()
+
+    if current_project:
+        projects.append(current_project.strip())
+
+    filtered_projects = [project for project in projects if len(project.split()) > 5]
+
+    return filtered_projects
+
+def extract_courses_certifications_achievements(cv_text):
+    sections = []
+    
+    course_keywords = ['course', 'certification', 'training', 'online course', 'workshop']
+    achievement_keywords = ['achievement', 'award', 'recognition', 'honor', 'milestone']
+    
+    lines = cv_text.split('\n')
+    current_section = None
+    current_section_type = None
+
+    for line in lines:
+        line_lower = line.lower().strip()
+
+        if any(keyword in line_lower for keyword in course_keywords):
+            if current_section:
+                sections.append({'type': current_section_type, 'content': current_section.strip()})
+            current_section = line.strip()
+            current_section_type = 'Courses/Certifications'
+        elif any(keyword in line_lower for keyword in achievement_keywords):
+            if current_section:
+                sections.append({'type': current_section_type, 'content': current_section.strip()})
+            current_section = line.strip()
+            current_section_type = 'Achievements'
+        elif current_section:
+            current_section += " " + line.strip()
+
+    if current_section:
+        sections.append({'type': current_section_type, 'content': current_section.strip()})
+    
+    return sections
+
+def extract_work_experience(cv_text):
+    work_experience = []
+    
+    work_experience_keywords = ['work experience', 'employment history', 'professional experience', 'job', 'company', 'position', 'role']
+    
+    lines = cv_text.split('\n')
+    current_experience = None
+
+    for line in lines:
+        line_lower = line.lower().strip()
+        
+        if any(keyword in line_lower for keyword in work_experience_keywords) and line:
+            if current_experience:
+                work_experience.append(current_experience.strip())
+            current_experience = line.strip()
+        elif current_experience:
+            current_experience += " " + line.strip()
+
+    if current_experience:
+        work_experience.append(current_experience.strip())
+
+    return work_experience
--- Backend/app/routes/candidate_routes.py
+++ Backend/app/routes/candidate_routes.py
@@ -1,10 +1,31 @@
 from flask import Blueprint, request, jsonify, send_from_directory
 from app.models.candidate_model import Candidate
-from app.cv_processing import extract_text_from_pdf, extract_contact_info, extract_soft_skills, extract_technical_skills_from_projects, generate_and_store_skills_chart
+from app.cv_processing import (
+    extract_text_from_pdf,
+      extract_contact_info, 
+      extract_soft_skills, 
+      extract_technical_skills_from_projects, 
+      generate_and_store_skills_chart,
+      extract_programming_languages,
+      extract_website_development_technologies,
+      extract_programming_frameworks,
+      extract_cloud_technologies,
+      extract_devops_technologies,
+      extract_version_control_technologies,
+      extract_database_technologies,
+      extract_project_experiences,
+      extract_courses_certifications_achievements,
+      extract_work_experience,
+      extract_software_development_methodologies
+)
 import os
 import json
 from collections import Counter
 from app.similarity import calculate_similarity
+from app.utils.chart_utils import get_and_display_chart
+from app.models.job_model import Job
+import joblib
+import pandas as pd
 
 candidate_routes = Blueprint('candidates', __name__)
 
@@ -30,12 +51,40 @@ def create_candidate():
         # Extract and process CV data
         cv_text = extract_text_from_pdf(cv_path)
         contact_info = extract_contact_info(cv_text)
+        no_devops_tools = extract_devops_technologies(cv_text)
+        no_cloud_technologies = extract_cloud_technologies(cv_text)
+        no_programming_frameworks = extract_programming_frameworks(cv_text)
+        no_web_technologies = extract_website_development_technologies(cv_text)
+        no_programming_languages = extract_programming_languages(cv_text)
+        no_version_control_technologies = extract_version_control_technologies(cv_text)
+        no_database_technologies = extract_database_technologies(cv_text)
+        no_software_development_methodologies = extract_software_development_methodologies(cv_text)
+
+        project_experiences = extract_project_experiences(cv_text)
+        courses_certifications_achievements = extract_courses_certifications_achievements(cv_text)
+        work_experience = extract_work_experience(cv_text)
+
 
         # Add processed data to the candidate data
         data.update({
             'extracted_email': contact_info.get('email'),
             'extractedgithub': contact_info.get('github'),
             'extractedlinkedin': contact_info.get('linkedin'),
+            'extractednoofprogramminglanguages': no_programming_languages,
+            'extractednoofwebtechnologies': no_web_technologies,
+            'extractednoofprogrammingframeworks': no_programming_frameworks,
+            'extractednoofcloudtechnologies': no_cloud_technologies,
+            'extractednoofdevopstools': no_devops_tools,
+            'extractednoofversioncontroltechnologies': no_version_control_technologies,
+            'extractednoofdatabasetechnologies': no_database_technologies,
+            'project_experiences': project_experiences,
+            'extractednoofsoftwaredevelopmentmethodologies': no_software_development_methodologies,
+
+            'courses_certifications_achievements': courses_certifications_achievements,
+            'work_experience': work_experience,
+
+
+        
             'cv_text': cv_text
         })
 
@@ -53,9 +102,105 @@ def create_candidate():
     candidate = Candidate.create(data)
 
     # Trigger post-save function to process CV further
-    process_candidate_data(candidate['_id'])
+    process_similarity_candidate_data(candidate['_id'])
 
     return jsonify(candidate), 201
+# Load the model
+model_path = os.path.join(os.getcwd(), "local_model", "stacked_model.joblib")
+model = joblib.load(model_path)
+
+feature_columns_path = os.path.join(os.getcwd(), "local_model", "feature_columns.pkl")
+feature_columns = joblib.load(feature_columns_path)
+
+def process_similarity_candidate_data(candidate_id):
+    # Fetch the candidate data from the database
+    candidate = Candidate.get_by_id(candidate_id)
+    if not candidate:
+        return jsonify({"error": "Candidate not found"}), 404
+
+    # Extract and prepare candidate data
+    courses_certifications_achievements = candidate.get("courses_certifications_achievements", [])
+    work_experience = candidate.get("work_experience", [])
+    project_experiences = candidate.get("project_experiences", [])
+
+    # Ensure the candidate data is properly formatted into strings
+    courses_certifications_text = " ".join(item.get("content", "") if isinstance(item, dict) else item for item in courses_certifications_achievements)
+    work_experience_text = " ".join(work_experience)
+    projects_text = " ".join(project_experiences)
+
+    if not (courses_certifications_text.strip() or work_experience_text.strip() or projects_text.strip()):
+        return jsonify({"error": "Candidate data missing for similarity calculation"}), 400
+    
+
+# Initialize all expected feature columns to 0
+    candidate_data = {col: 0 for col in feature_columns}
+
+    candidate_data = {
+       "Years of Experience": candidate.get("noofyearsofexperience", 0),
+        "Roles Marks": candidate.get("roles_marks", 0),
+        "Programming Languages": candidate.get("extractednoofprogramminglanguages", 0),
+        "Programming Frameworks": candidate.get("extractednoofprogrammingframeworks", 0),
+        "Software Dev Methods": candidate.get("extractednoofsoftwaredevelopmentmethodologies", 0),
+        "Web Dev Technologies": candidate.get("extractednoofwebtechnologies", 0),
+        "Database Technologies": candidate.get("extractednoofdatabasetechnologies", 0),
+        "Version Control": candidate.get("extractednoofversioncontroltechnologies", 0),
+        "Courses & Certifications Matching %": candidate.get("coursesAndCertificationMatchingSimilarity", 0),
+        "Achievements Matching %": candidate.get("achievements_similarity", 0),
+        "Work Experience Matching %": candidate.get("workExperienceMatchingSimilarity", 0),
+        "Projects Matching %": candidate.get("projectsMatchingSimilarity", 0),
+        "Cloud Technologies": candidate.get("extractednoofcloudtechnologies", 0),
+        "CI/CD Technologies": candidate.get("extractednoofdevopstools", 0),
+        f"Position_{candidate.get('jobPosition', '')}": 1,
+        f"Education Level_{candidate.get('education_level', '')}": 1,
+        f"Specialization_{candidate.get('specialization', '')}": 1,
+        f"University_{candidate.get('university', '')}": 1
+    }
+    
+    # Convert to DataFrame and reindex to match the model's feature columns
+    input_df = pd.DataFrame([candidate_data])
+    input_df = input_df.reindex(columns=feature_columns, fill_value=0)
+
+    # Predict matching percentage
+    try:
+        prediction = model.predict(input_df)[0]
+    except Exception as e:
+        return jsonify({"error": f"Prediction failed: {str(e)}"}), 500
+
+    # Extract job details using jobID
+    job_id = candidate.get("jobID")
+    if not job_id:
+        return jsonify({"error": "Job ID not associated with candidate"}), 400
+
+    job = Job.get_by_id(job_id)
+    if not job:
+        return jsonify({"error": "Job not found"}), 404
+
+    # Fetch and validate job details
+    qualifications = job.get("qualifications", "")
+    required_skills = job.get("requiredSkills", "")
+    duties = job.get("duties", "")
+    experience = job.get("experience", "")
+
+    if not (qualifications.strip() and required_skills.strip() and duties.strip() and experience.strip()):
+        return jsonify({"error": "Job details incomplete for similarity calculation"}), 400
+
+    # Perform similarity calculations
+    courses_and_certifications_similarity = calculate_similarity(courses_certifications_text, qualifications)
+    work_experience_similarity = calculate_similarity(work_experience_text, experience)
+    projects_similarity = calculate_similarity(projects_text, required_skills)
+
+    # Update the candidate record in the database
+    update_data = {
+        "coursesAndCertificationMatchingSimilarity": float(courses_and_certifications_similarity),
+        "workExperienceMatchingSimilarity": float(work_experience_similarity),
+        "projectsMatchingSimilarity": float(projects_similarity),
+        "predicted_matching_percentage": round(prediction, 2)
+    }
+    Candidate.update(candidate_id, update_data)
+
+    return jsonify({"message": "Similarity scores updated successfully"}), 200
+
+
 
 
 def process_candidate_data(candidate_id):
@@ -88,6 +233,12 @@ def process_candidate_data(candidate_id):
         'skills_chart_image': chart_image_base64  # Store the base64-encoded image
     }
     Candidate.update(candidate_id, update_data)
+    
+@candidate_routes.route('/candidates/<candidateID>/process', methods=['POST'])
+def process_candidate(candidateID):
+    process_candidate_data(candidateID)
+    return jsonify({"message": "Candidate processing triggered successfully"}), 200
+
 
     
 
@@ -117,4 +268,8 @@ def serve_cv(filename):
 def serve_transcript(filename):
     return send_from_directory(UPLOAD_FOLDER_TRANSCRIPTS, filename)
 
+@candidate_routes.route('/candidates/<candidateID>/chart', methods=['GET'])
+def display_candidate_chart(candidateID):
+    get_and_display_chart(candidateID)
+    return jsonify({"message": "Chart displayed successfully"}), 200
 
